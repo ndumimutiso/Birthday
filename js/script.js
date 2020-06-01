d5db923b8a7d5a04e3c8e1759490c756dc3cfcf0
Jacinta Ndumi Mutiso <JMutiso@safaricom.co.ke>
2020-06-01 16:43:22 +0300

    debugged errors on determine the birth day of the week

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
 function findAge() {
-	var date = document.getElementById('birthday').value;
-	var gender = document.getElementById('gender').value;
+	var dt = document.getElementById('birthday').value;
+	var today = new Date();
+	var date = new Date(dt);
 	var month = date.getMonth();
-	var day = date.getdate();
+	var day = date.getDate();
 	var year = date.getFullYear();
 	var CC=Math.ceil(year/100);
-
 	if(date === ""){
 		alert("Please complete the required field!");
 	}else if (month >12||month<1){
 								alert("Invalid Month!");
-	 }else if (day >31||day<1){
-								alert("Invalid Day!");
-  }else{
-
-var dday= ( ( (CC/4) -2*CC-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7;
-
-
-		}
-		document.getElementById('result').innerHTML = "<center><h2>Congratulation</h2></center> <center><label>You birth day was on a "+dday+"</label></center>";
+	}else if (day >31||day<1){
+ 							alert("Invalid Day!");
+	}else{
+			var dday= ( ( (CC/4) -2*CC-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7;
+		document.getElementById('result').innerHTML = "<center><h2>Congratulation</h2></center> <center><label>You are now "+dday+"yrs old today</label></center>";
 	}
 }
