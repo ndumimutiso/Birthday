3ff452f53ad740a0f7a652b735e5ae8fbf379329
ndumimutiso <ndumimutiso@gmail.com>
2020-06-01 21:39:10 +0300

    Added Ghanaian Names

--- js/script.js
+++ js/script.js
@@ -22,31 +22,64 @@ function findAge() {
  							alert("Invalid Day!");
 	}else{
 			var dday= (((CC/4) - 2*CC-1) + ((5*year)/4) + ((26*(month+1)/10)) + day) % 7;
-			var d=Math.trunc(dday);
-			var dow="Tuesday";
+			var d=Math.trunc(dday);var dow="";
 			uname="";
 			if(d=="1")
+			{
+				dow="Monday";
+				if(uGender="male")
+					uname="Kwadwo";
+				else
+					uname="Adwoa";
+			}	
+			else if(d=="2")
+			{
+				dow="Tuesday";
+				if(uGender="male")
+					uname="Kwabena";
+				else
+					uname="Abenaa";
+			}	
+			else if(d=="3")
+			{
+				dow="Wednesday";
+				if(uGender="male")
+					uname="Kwaku";
+				else
+					uname="Akua";
+			}	
+			else if(d=="4")
 				{
-					dow="Mondday";
-if(uGender="male")
-{
-	uname="";
-}
-			}
-				else 	if(d=="1")
-						dow="Monday";
-						else 	if(d=="2")
-								dow="Tuesday";
-								else 	if(d=="3")
-										dow="Wednesday";
-										else 	if(d=="4")
-												dow="Thursday";
-												else 	if(d=="5")
-														dow="Friday";
-														else 	if(d=="6")
-																dow="Saturday";
-																else
-																	dow="Sunday";
+				dow="Thursday";
+				if(uGender="male")
+					uname="Yaw";
+				else
+					uname="Yaa";
+			}	
+			else if(d=="5")
+			{
+				dow="Friday";
+				if(uGender="male")
+					uname="Kofi";
+				else
+					uname="Afua";
+			}	
+			else if(d=="6")
+			{
+				dow="Saturday";
+				if(uGender="male")
+					uname="Kwame";
+				else
+					uname="Ama";
+			}	
+			else
+			{
+				dow="Sunday";
+				if(uGender="male")
+					uname="Kwasi";
+				else
+					uname="Akosua";
+			}	
 		document.getElementById('result').innerHTML = "<center><h2>You were born on a "+dow+"</h2></center> <center><label>Your akan name is "+dow+" </label></center>";
 	}
 }
